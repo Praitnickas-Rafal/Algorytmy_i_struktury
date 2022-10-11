0e5cf8529ef0cb930c930d61d74be8e9ba2036fd
Praitnickas-Rafal <115026306+Praitnickas-Rafal@users.noreply.github.com>
2022-10-11 23:44:24 +0300

    Create Zadanie4.c

---
+++ Laboratorium/LAB1-zadanie/Zadanie4.c
@@ -0,0 +1,32 @@
+/*
+ Rafal Praitnickas, Student
+******************************************
+ Laboratorium 1. Obliczyc funkcja y = ax^2 znaczenia, gdy a - stale, x jest wybierany z tablicy X(n), wyniki funckji zapisywac Y(n) 
+****************************************** 
+2022.10.11
+*/
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+	
+    int n;
+    double a, x[n], y[n];
+    printf("Prosze, podac dlugosc tablicy:");
+    scanf("%d", &n);
+    
+    printf("Prosze, podac a:");
+    scanf("%lf", &a);
+    
+    for(int i=0; i < n; i++)
+    {
+        printf("Podaj x%d:", i+1);
+        scanf("%lf", &x[i]);
+    }
+    for(int i=0; i < n; i++)
+    {
+        y[i] = (a*x[i]*x[i]); //zapisuji do tablicy
+        printf("Wynik: y = %.lf , a = %.lf , x z Tablicy = %.lf \n", y[i], a, x[i]);
+    }
+    }
