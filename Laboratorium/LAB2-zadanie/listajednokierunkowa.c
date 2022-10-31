f5c2c4c19bed8f8a24333daf2448e1daba7b0c4b
Rafal Praitnickas <115026306+Praitnickas-Rafal@users.noreply.github.com>
2022-10-31 18:54:33 +0200

    Update listajednokierunkowa.c

--- Laboratorium/LAB2-zadanie/listajednokierunkowa.c
+++ Laboratorium/LAB2-zadanie/listajednokierunkowa.c
@@ -3,19 +3,21 @@
 // 2022-10-24
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+
 
 typedef struct Item {
   float value;
   struct Item *next;
 } ItemType;
 
-int find(float value, int *head);
+int find(float value, ItemType *head); //zmieniłem int *head na itemtype *head
 
-int locate(float value, int *head); //??
+int locate(float value, int *head); //???
 
-int retrieve(ItemType *item, int *head);
+int retrieve(ItemType *item, int *head); //???
 
-void insert(ItemType **head, float value, int cyfra,
+void insert(ItemType **head, float value, 
             int position); // zmieniłem i dobawiłem
 
 void remove1(ItemType **head,
@@ -24,13 +26,13 @@ void remove1(ItemType **head,
 
 int size(ItemType *head); // zmieniłem int na ItemType
 
-ItemType findMax(int *head);
+int findMax(ItemType **head); //zmieniłem i dobawiłem
 
-ItemType findMin(int *head);
+int findMin(ItemType *head);  //zmieniłem i dobawiłem
 
-void printList(int *head); // zmieniłem na void
+void printList(ItemType *head); // zmieniłem na void
 
-int isEmpty(int *head);
+int isEmpty(ItemType *head); //zmieniłem i dobawiłem
 
 void next(ItemType **head,
           int cyfra); //?? next co?? czyli na koniec dodaj element? zmieniłem
@@ -49,11 +51,70 @@ int main(void) {
   head = (ItemType *)malloc(sizeof(ItemType));
   head = NULL;
 
-  printList(head);
-  previous(&head, 6);
-  printList(head);
-  previous(&head, 5);
+  /*-----------------------------------------------------*/
+  
+    previous(&head, 6);    //sprawdzamy funkcyji previuos() i printlist()
+    previous(&head, 5);
+    previous(&head, 8);
+    printList(head);
+  
+  /*-----------------------------------------------------*/
+  
+    int i = size(head);     //sprawdzamy funkcje size()
+    printf("%d \n", i);
+  
+  /*-----------------------------------------------------*/
+  
+    remove1(&head, 0);      //sprawdzamy funkcyje deleteElement(head, position)
+    printList(head);
+  
+  /*-----------------------------------------------------*/
+  
+    next(&head, 5);        //sprawdzamy funkcyje next(head, number)
+    printList(head);
+  
+  /*-----------------------------------------------------*/
+  
+    insert(&head, 7, 1);   //sprawdzamy funkcyje insert(head, number, position)
+    printList(head);
 
+  /*-----------------------------------------------------*/
+  
+    int s = isEmpty(head); // s - rezultat //sprawdzamy funkcyje isEmpty(head)
+  
+    if (s == 1){
+      printf("\n The list is not empty: \n");
+    }
+    else
+    {
+      printf("\n List is empty: \n");
+    }  
+  
+  /*-----------------------------------------------------*/
+  
+  int index = find(5.0, head);      //sprawdzamy funkcyje find()
+    
+    if(index == -1)
+        printf("\n Item not found \n");
+    else
+        printf("\n Item found at position: %d \n", index+1);
+    
+  /*-----------------------------------------------------*/
+
+   int max = findMax(&head);       //sprawdzamy funkcyje findMax()
+   printf("\n max: %d \n", max);
+
+  /*-----------------------------------------------------*/
+   
+   int min = findMin(head);        //sprawdzamy funkcyje findMin()
+   printf("\n min: %d \n", min);
+
+  /*-----------------------------------------------------*/
+         
+  
+
+
+  
   return 0;
 }
 
@@ -71,14 +132,14 @@ int size(ItemType *head) {
   return counter; // test
 }
 
-void printList(int *head) {
+void printList(ItemType *head) {
   printf("\n");
   if (head == NULL) {
     printf("List is empty");
   } else {
     ItemType *current = head;
     do {
-      printf("%f", current->value);
+      printf("%.f", current->value);
       printf("\n");
       current = current->next;
     } while (current != NULL);
@@ -86,13 +147,14 @@ void printList(int *head) {
 }
 void remove1(ItemType **head, int position) {
   if (position == 0) {
-    ItemType *tmp = NULL;
+    ItemType * tmp = NULL;
 
     if (*head != NULL) {
       tmp = (*head)->next;
       free(*head);
       *head = tmp;
-    } else {
+    } }
+    else {
       ItemType *current = *head;
       ItemType *tmp;
       int i = 0;
@@ -106,7 +168,7 @@ void remove1(ItemType **head, int position) {
       free(tmp);
     }
   }
-}
+
 void previous(ItemType **head, float value) {
 
   ItemType *current;
@@ -133,12 +195,12 @@ void next(ItemType **head, int cyfra) {
   }
 }
 
-void insert(ItemType **head, float value, int cyfra, int position) {
+void insert(ItemType **head, float value, int position) {
   if (position == 0)
-    previous(head, cyfra);
+    previous(head, value);
   else {
     if (position == size(*head))
-      next(head, cyfra);
+      next(head, value);
     else {
       ItemType *current = *head;
       ItemType *tmp;
@@ -151,8 +213,62 @@ void insert(ItemType **head, float value, int cyfra, int position) {
 
       tmp = current->next;
       current->next = (ItemType *)malloc(sizeof(ItemType));
-      current->next->value = cyfra;
+      current->next->value = value; //
       current->next->next = tmp;
     }
   }
+}
+int isEmpty(ItemType *head)
+{
+   if(head == NULL)
+   {
+     return 0;
+   }
+   else
+   {
+     return 1;
+   }
+  
+}
+int find(float value, ItemType *head)
+{
+  ItemType *current = head;
+  int index = 0;
+
+  while (current != NULL)
+    {
+      if(current->value == value){
+        return index;
+      }
+      current = current->next;
+      index++;
+    }
+    return -1;
+}
+
+int findMax(ItemType **head)
+{
+    ItemType *current;
+    int max=0;
+    current = *head;
+    
+    
+    while(current!=NULL) {
+    if(current->value>max) {
+        max=current->value;
+    }
+    current=current->next;
+}
+  return max;
+}
+int findMin(ItemType *head)
+{
+    
+    int min=head->value;
+
+    while(head->value < min){
+      min = head -> value;
+      head=head->next;
+      }
+      return min;
 }
