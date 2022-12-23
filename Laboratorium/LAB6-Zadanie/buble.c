07482b68dacf839c5a109d33f690017566214954
Rafal Praitnickas <115026306+Praitnickas-Rafal@users.noreply.github.com>
2022-12-23 18:12:37 +0200

    Create buble.c

---
+++ Laboratorium/LAB6-Zadanie/buble.c
@@ -0,0 +1,16 @@
+//Wykonał: Rafal Praitnicas, Student
+//Data: 2022.12.23
+#include <stdio.h>
+
+int bubble(int *arr, int s) {
+  for (int i = 0; i < s - 1; i++) 
+  {
+    for (int j = s - 1; j > i; j--) 
+    {
+      if (prior(arr, j, j - 1)) 
+      {
+        swap(arr, j, j - 1);
+      }
+    }
+  }
+}
