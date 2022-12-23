dd26a173ebe8fdf7790316199fbbf5b54c7745f6
Rafal Praitnickas <115026306+Praitnickas-Rafal@users.noreply.github.com>
2022-12-23 18:41:42 +0200

    Create wymiania.c

---
+++ Laboratorium/LAB6-Zadanie/wymiania.c
@@ -0,0 +1,19 @@
+//Wykonał: Rafal Praitnickas, Student
+//Data: 2023.12.23
+#include <stdio.h>
+
+int selection(int *arr, int n) 
+{
+for (i= 0; i < n - 1; i++)
+   {
+    int lowIndex = i;
+    for (int j = n-1; j > i; j--) 
+    {
+     	if (prior(arr[j], arr[lowIndex])) 
+     	{
+	lowIndex = j;
+	swap(arr, i, lowIndex)
+     	}
+    }
+   }
+}
