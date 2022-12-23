fd20db85bbbfca3fb1d674a8f842dded7188c7db
Rafal Praitnickas <115026306+Praitnickas-Rafal@users.noreply.github.com>
2022-12-23 18:22:52 +0200

    Create QuickSort.c

---
+++ Laboratorium/LAB6-Zadanie/QuickSort.c
@@ -0,0 +1,26 @@
+//Wykonał: Rafal Praitnickas
+//Data: 2023.12.23
+#include <stdio.h>
+
+void quickSort(int *arr, int left, int right) {
+  int i, j, pivot, temp;
+  if (left < right) {
+    pivot = left;
+    i = left;
+    j = right;
+    while (i < j) {
+      while (arr[i] <= arr[pivot] && i < right)
+        i++;
+      while (arr[j] > arr[pivot])
+        j--;
+      if (i < j) {
+        swap(arr, i, j);
+      }
+    }
+    temp = arr[pivot];
+    arr[pivot] = arr[j];
+    arr[j] = temp;
+    quickSort(arr, left, j - 1);
+    quickSort(arr, j + 1, right);
+  }
+}
