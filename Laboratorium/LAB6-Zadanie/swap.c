71f2348bc3ffd74b33796bb61c15f2f548e8c5cc
Rafal Praitnickas <115026306+Praitnickas-Rafal@users.noreply.github.com>
2022-12-23 18:02:40 +0200

    Update swap.c

--- Laboratorium/LAB6-Zadanie/swap.c
+++ Laboratorium/LAB6-Zadanie/swap.c
@@ -1,1 +1,9 @@
+//Wykonał: Rafal Praitnickas, Student
+//Data: 2022-12-23
+#include <stdio.h>
 
+int swap(int *arr, int X, int Y) {
+  int temp = arr[X];
+  array[index1] = arr[Y];
+  array[index2] = temp;
+}
